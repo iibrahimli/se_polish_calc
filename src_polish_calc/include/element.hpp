555ea2964f89884546f911178418978e56f1c47d
iibrahimli <imranibrahimli98@gmail.com>
2019-03-16 18:47:43 +0400

    add _get_operands_from_stack and basic arithmetic

--- src_polish_calc/include/element.hpp
+++ src_polish_calc/include/element.hpp
@@ -2,15 +2,18 @@
 #define ELEMENT_HPP
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #include <unordered_map>
+#include <stack>
 #include <cmath>
 
 
 namespace pc{
     union element;
     size_t num_operands(char op);
-    size_t num_operands(element el);
-    std::pair<double, bool> eval_op(char op, element op1, element op2);
+    bool eval_op(char op, std::stack<element>& opst);
+    std::vector<double> _get_operands_from_stack(std::stack<element>& st, size_t n_ops);
 
      // a map from operations to number of operands
     std::unordered_map<char, size_t> _n_operands({
@@ -21,7 +24,6 @@ namespace pc{
         {'^', 2},
         {'r', 1}
     });
-
 };
 
 
@@ -68,22 +70,76 @@ size_t pc::num_operands(char op){
 }
 
 
-// evaluates and returns a pair containing the result (number) and a boolean
-//indicating whether the operation was successfully evaluated or not
-std::pair<double, bool>  pc::eval_op(char op, std::stack<element>& opst){
-    std::pair<double, bool> res;
+// gets N_OPS elements from the top of ST (from oldest to newest)
+std::vector<double> pc::_get_operands_from_stack(std::stack<element>& st, size_t n_ops){
+    std::vector<double> ops;
+
+    // check whether there are enough numbers in the stack
+    if(st.size() < n_ops){
+        std::runtime_error("Stack contains less numbers than needed");
+        return ops;
+    }
+
+    for(int i=0; i<n_ops; ++i){
+        ops.push_back(st.top()._num);
+        st.pop();
+    }
+
+    // reverse the order so that the oldest element is first in the vector
+    std::reverse(ops.begin(), ops.end());
+
+    return ops;
+}
+
+
+// evaluates the operation modifying the stack as necessary (popping operands
+// and pushing the result) and returns boolean indicating success
+bool  pc::eval_op(char op, std::stack<element>& opst){
     auto oper = _n_operands.find(op);
     if( oper != _n_operands.end()){
         // op is a valid operation
-        switch(oper->first){
+        
+        double res = 0;
+        auto operands = _get_operands_from_stack(opst, num_operands(op));
 
+        switch(oper->first){
+            case '+':
+            // addition
+            
+                break;
+
+            case '-':
+            // subtraction
+
+                break;
+
+            case '*':
+            // multiplication
+
+                break;
+
+            case '/':
+            // division
+
+                break;
+
+            case '^':
+            // power
+
+                break;
+
+            case 'r':
+            // sqrt
+
+                break;
         }
+
+        return true;
     }
     else{
         std::runtime_error("Unsupported operation character: " + std::to_string(op));
-        res = {0, false};
+        return false;
     }
-    return res;
 }
 
 
